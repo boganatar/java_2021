0bde42f4fcd36db9609e85ff4e845de6c00bb84e
andriil <andrii.levchenko@kstt.ua>
2022-01-23 12:41:52 +0200

    Homework done

--- hw02-generics/src/main/java/homework/Customer.java
+++ hw02-generics/src/main/java/homework/Customer.java
@@ -1,6 +1,8 @@
 package homework;
 
-public class Customer {
+import org.jetbrains.annotations.NotNull;
+
+public class Customer implements Comparable<Customer>{
     private final long id;
     private String name;
     private long scores;
@@ -11,6 +13,12 @@ public class Customer {
         this.id = id;
         this.name = name;
         this.scores = scores;
+    }
+
+    public Customer(Customer customer) {
+        this.id = customer.getId();
+        this.name = customer.getName();
+        this.scores = customer.getScores();
     }
 
     public long getId() {
@@ -46,19 +54,19 @@ public class Customer {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Customer customer = (Customer) o;
 
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return name != null ? name.equals(customer.name) : customer.name == null;
+        return id == customer.id;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
-        return result;
+        return (int) (id ^ (id >>> 32));
     }
+
+    @Override
+    public int compareTo(@NotNull Customer customer) {
+        return Long.compare(this.scores, customer.scores);
+    }
+
 }
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,35 @@
 package homework;
 
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    NavigableMap<Customer, String> customersTreeMap = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> smallestCustomer = customersTreeMap.firstEntry();
+        if (smallestCustomer == null) {
+            return null;
+        } else {
+            return new AbstractMap.SimpleEntry<>(new Customer(smallestCustomer.getKey()), smallestCustomer.getValue());
+        }
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer,String> nextCustomer = customersTreeMap.higherEntry(customer);
+        if (nextCustomer == null) { return null; } else {
+            return new AbstractMap.SimpleEntry<>(new Customer(nextCustomer.getKey()), nextCustomer.getValue());
+        }
     }
 
     public void add(Customer customer, String data) {
-
+        customersTreeMap.put(customer, data);
     }
 }
